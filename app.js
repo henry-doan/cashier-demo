e6b7f5834b6dafc2b751af77896ab4fae283ccf7
nightwing891 <henrydoan96@gmail.com>
2015-06-18 17:39:26 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+var app = angular.module('app', []);
+
+app.controller('myCtrl', ['$scope', function($scope){
+	$scope.orderByField = 'title';
+  $scope.reverseSort = false;
+
+	$scope.transactions = [
+		{
+			title: 'Sloppy Joe',
+			cost: 4.49,
+			notes: 'Extra sloppy'
+		},
+		{
+			title: 'Crabby patty',
+			cost: 3.49,
+			notes: 'No pickles'
+		},
+		{
+			title: 'Hot dog',
+			cost: 2.49,
+			notes: 'Vegan'
+		},
+		{
+			title: 'Soda',
+			cost: 1.49,
+			notes: 'Large'
+		},
+		{
+			title: 'Skittles',
+			cost: 9.49,
+			notes: 'Taste the rainbow'
+		}
+	];
+}]);
